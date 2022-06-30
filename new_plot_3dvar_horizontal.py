60e6a85cb7d5547f0fa2086b9a2b8dc07ba8b9f8
V <44793625+LucianoAndrian@users.noreply.github.com>
2022-06-30 18:09:16 -0300

    primeras figuras
    scripts adaptados de la clase

---
+++ new_plot_3dvar_horizontal.py
@@ -0,0 +1,104 @@
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+import glob
+import scipy.io as sio
+import cartopy.feature
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+import cartopy.crs as ccrs
+import os
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+import warnings
+#warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
+warnings.filterwarnings('ignore')
+plt.switch_backend('agg')
+########################################################################################################################
+files_dir = './tp_final/WRF_CLM4/'
+out_dir = './plots/'
+########################################################################################################################
+
+from wrf import getvar, interplevel, to_np, get_basemap, latlon_coords
+
+plot_time = 58     #En que tiempo vamos a hacer el grafico (arrancando desde 0)
+figure_name = 'T_uv'   #Un nombre que distinga esta figura de otros tipos de figuras.
+nivel      = 1000      #Altura del nivel (m) a donde interpolaremos los datos.
+plot_mat   = True     #Si es True grafica el mapa, sino ponerlo en False
+
+file_list = glob.glob(files_dir + 'wrfout_d02_*')   #Busco todos los wrfout en la carpeta indicada.
+file_list.sort()
+ntimes = len( file_list ) #Encuentro la cantidad de tiempos disponibles.
+
+for plot_time in range(60, len(file_list)):
+    ncfile = Dataset(file_list[plot_time])
+
+    # z = getvar(ncfile, "height",units='m')
+    # [um , vm] = getvar(ncfile, "uvmet", units="m s-1")
+    tk = getvar(ncfile, "PBLH")
+    # tk2 = getvar(ncfile, "RAINNC")
+    # tk += tk2
+
+    # Interpolamos verticalmente a la altura seleccionada
+    # um_z = interplevel(um, z, nivel)
+    # vm_z = interplevel(vm, z, nivel)
+    # t_z  = interplevel(tk, z, nivel)
+
+    # Calculo la velocidad del viento
+    # wspd_z = np.sqrt(um_z**2 + vm_z**2)
+    t_z = tk
+    # Obtenemos las lat y lons correspondientes a nuestras variables.
+    lats, lons = latlon_coords(t_z)
+    lats = to_np(lats)
+    lons = to_np(lons)
+    # Nota: Por defecto wrfpython genera variables que son objetos Xarray estos son
+    # tipos de datos y metadatos. Para convertir los datos a arrays de numpy esta la
+    # funcion to_np que toma el Xarray, extrae los datos como un array de numpy.
+
+    # Creamos la figura.
+    dpi = 50
+    fig = plt.figure(figsize=(4, 4), dpi=dpi)
+    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+    crs_latlon = ccrs.PlateCarree()
+    ax.set_extent([-60, -57, -33, -36.5], crs_latlon)
+
+    # Graficamos la temperatura en contornos
+    levels = np.arange(np.round(to_np(t_z).min()) - 2., np.round(to_np(t_z).max()) + 2., 2.)
+    im = ax.contourf(lons, lats, to_np(t_z), levels=np.linspace(0, 2000, 1000), cmap='Spectral_r', extend='both')
+    cb = plt.colorbar(im, fraction=0.042, pad=0.035, shrink=0.8)
+    cb.ax.tick_params(labelsize=8)
+    ax.add_feature(cartopy.feature.LAND, facecolor='lightgrey')
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.STATES)
+    ax.gridlines(crs=crs_latlon, linewidth=0.3, linestyle='-')
+    ax.set_xticks(np.arange(-60, -57, 10), crs=crs_latlon)
+    ax.set_yticks(np.arange(-33, -36, 6), crs=crs_latlon)
+    lon_formatter = LongitudeFormatter(zero_direction_label=True)
+    lat_formatter = LatitudeFormatter()
+    ax.xaxis.set_major_formatter(lon_formatter)
+    ax.yaxis.set_major_formatter(lat_formatter)
+    ax.tick_params(labelsize=7)
+    plt.title('RAINCC(mm)' + ' t = ' + str(plot_time))
+    plt.savefig(out_dir + 'pic' + str(plot_time) + '.png')
+    plt.close('all')
+
+#plt.show()
+# Agregamos los contornos de velocidad de viento.
+# levels = [1,5,10,15]
+# contour=plt.contour(lons,lats,to_np(wspd_z),levels=levels,colors='k')
+# plt.clabel(contour,inline=1, fontsize=10, fmt="%i")
+
+# Agregamos el viento en barbas pero graficando solo cada 10 puntos de reticula.
+skip=10
+# plt.barbs(lons[::skip,::skip],lats[::skip,::skip],to_np(um_z[::skip, ::skip]),to_np(vm_z[::skip, ::skip]),length=6)
+
+
+#Agregamos las gridlines
+#plt.grid()
+#Ajustamos los limites de la figura al dominio del WRF
+#plt.axis( [ lons.min() , lons.max() , lats.min() , lats.max() ] )
+#Agregamos un titulo para la figura
+
+#Guardamos la figura en un archivo, el nombre del archivo incluye el tiempo y el nivel asi como el figure_name que definimos al principio
+#plt.savefig( './' + figure_name + '_tiempo_' + str( plot_time ) + '_altura_' + str( nivel ) + '.png' )
+#El plt.show es opcional solo si se desea ver la figura en tiempo real. Sino se guarda automaticamente la figura en el archivo y no se
+#muestra por pantalla.
---
+++ new_plot_time_evolution.py
@@ -0,0 +1,135 @@
+import matplotlib.pyplot as plt
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+import cartopy.crs as ccrs
+import os
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+import warnings
+warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
+warnings.filterwarnings('ignore')
+plt.switch_backend('agg')
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.feature
+from netCDF4 import Dataset
+from wrf import (getvar, to_np, ll_to_xy , latlon_coords  )
+import glob
+import scipy.io as sio
+########################################################################################################################
+files_dir = './tp_final/WRF_CLM4/'
+files_dir2 = './tp_final/WRF_Noah-MP/'
+files_dir3 = './tp_final/WRF_RUC/'
+out_dir = './plots/'
+########################################################################################################################
+
+from wrf import getvar, interplevel, to_np, get_basemap, latlon_coords
+file_list = glob.glob(files_dir + 'wrfout_d02_*')
+
+file_list2 = glob.glob(files_dir2 + 'wrfout_d02_*')
+file_list2.sort()
+
+file_list3 = glob.glob(files_dir3 + 'wrfout_d02_*')
+file_list3.sort()
+
+ntimes = len( file_list )
+
+ncfile = Dataset(file_list[ 0 ])
+
+point_lon = -59
+point_lat = -34.5
+
+#Convierto el punto de latitud y longitud en x e y de la reticula del WRF.
+[point_x,point_y]=ll_to_xy(ncfile, point_lat, point_lon, meta=False)
+
+ter = getvar(ncfile, "ter")   #Altura de la topografia
+
+
+v1 = np.zeros( ntimes )
+v2= np.zeros( ntimes )
+v3= np.zeros( ntimes )
+
+v11 = np.zeros( ntimes )
+v22= np.zeros( ntimes )
+v33= np.zeros( ntimes )
+
+time= np.zeros( ntimes )
+for ifile , my_file in enumerate( file_list ) :
+    ncfile1 = Dataset(file_list[ ifile ])
+    v1[ ifile ] = getvar(ncfile1, 'LH' )[point_y,point_x]
+
+    ncfile2 = Dataset(file_list2[ ifile ])
+    v2[ ifile ] = getvar(ncfile2, 'LH' )[point_y,point_x]
+
+    ncfile3 = Dataset(file_list3[ ifile ])
+    v3[ ifile ] = getvar(ncfile3, 'LH' )[point_y,point_x]
+
+    # ncfile1 = Dataset(file_list[ ifile ])
+    # v11[ ifile ] = getvar(ncfile1, 'RAINNC' )[point_y,point_x] + getvar(ncfile1, 'RAINC' )[point_y,point_x]
+    #
+    # ncfile2 = Dataset(file_list2[ ifile ])
+    # v22[ ifile ] = getvar(ncfile2, 'RAINNC' )[point_y,point_x] + getvar(ncfile2, 'RAINC' )[point_y,point_x]
+    #
+    # ncfile3 = Dataset(file_list3[ ifile ])
+    # v33[ ifile ] = getvar(ncfile3, 'RAINNC' )[point_y,point_x] + getvar(ncfile3, 'RAINC' )[point_y,point_x]
+
+
+    time[ ifile ]= getvar(ncfile3,'times')
+
+time = (time - time[0])/3600.0e9  #Pongo el tiempo en horas desde el inicio de la simulacion.
+
+# Obtenemos las matrices de latitud y longitud para los graficos.
+lats, lons = latlon_coords(ter)
+lats=to_np(lats)
+lons=to_np(lons)
+
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+fig = plt.figure(figsize=(10, 6), dpi=100)
+crs_latlon = ccrs.PlateCarree()
+ax1 = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=0))
+plot_ter = to_np(ter)
+plot_ter[ plot_ter <= 1.0 ] = np.nan
+ax1.contourf(lons,lats,plot_ter,levels=np.linspace(-100,300,1000), cmap='terrain',extend='max')
+ax1.plot( point_lon , point_lat , 'o' , color='r')
+ax1.set_extent([-60, -57, -33, -36.5], crs_latlon)
+ax1.add_feature(cartopy.feature.LAND, facecolor='lightgrey')
+ax1.add_feature(cartopy.feature.COASTLINE)
+ax1.add_feature(cartopy.feature.STATES)
+#ax1.gridlines(crs=crs_latlon, linewidth=0.3, linestyle='-', alpha=1)
+ax1.set_xticks(np.linspace(-60, -57, 6), crs=crs_latlon)
+ax1.set_yticks(np.linspace(-33, -36, 6), crs=crs_latlon)
+lon_formatter = LongitudeFormatter(zero_direction_label=True)
+lat_formatter = LatitudeFormatter()
+ax1.xaxis.set_major_formatter(lon_formatter)
+ax1.yaxis.set_major_formatter(lat_formatter)
+ax1.set_title('Ubicacion del meteograma')
+ax1.grid()
+
+
+divider = make_axes_locatable(ax1)
+ax2 = divider.new_horizontal(size="100%", pad=0.33, axes_class=plt.Axes)
+fig.add_axes(ax2)
+ax2.plot( time , v1 , 'r.-' ,label='CLM4')
+ax2.plot( time , v2 , 'b.-' ,label='Noah-MP')
+ax2.plot( time , v3 , 'g.-' ,label='RUC')
+ax2.grid()
+ax2.set_title('Meteograma LH (W m-2)')
+ax2.legend()
+#plt.plot( time , td2m,
+
+plt.tight_layout()
+plt.savefig(out_dir + 'pic' + '.png')
+
+# divider = make_axes_locatable(ax1)
+# ax3 = divider.new_horizontal(size="100%", pad=0.33, axes_class=plt.Axes)
+# fig.add_axes(ax3)
+# ax3.plot( time , v11 , 'r.-' ,label='CLM4')
+# ax3.plot( time , v22 , 'b.-' ,label='Noah-MP')
+# ax3.plot( time , v33 , 'g.-' ,label='RUC')
+# ax3.grid()
+# ax3.set_title('Meteograma PP acum (mm)')
+# ax3.legend()
+#
+# plt.tight_layout()
+# plt.savefig(out_dir + 'pic' + '.png')
